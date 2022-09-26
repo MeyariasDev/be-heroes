0227a60f327a46b70aa1da0414378da3d661d8b3
Alberto Acevedo <meyling.arias@contollo.net>
2022-09-25 21:25:56 -0600

    add fuction search by name

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
   async getHello(): Promise<string> {
     // se devuelve un index en strin convertido por la pagina
     // https://jsonformatter.org/json-stringify-online
-    // si sedea modificar el codigo vaya a view/index.html
+    // si desea modificar el codigo vaya a view/index.html
     // modifiquelo y copie todo el codigo en https://jsonformatter.org/json-stringify-online
     // y use el resultado convertido a string en el endpoint view
     const { response, exists } = await this.viewService.getViewIndex();
