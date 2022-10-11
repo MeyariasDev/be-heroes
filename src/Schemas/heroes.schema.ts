e4aafef1232602e2c541d5d6d5c8c02e08ec6537
MeyariasDev <devmey261296@gmail.com>
2022-10-11 14:42:45 -0600

    refactorizando Shema

--- src/Schemas/heroes.schema.ts
+++ src/Schemas/heroes.schema.ts
@@ -1,25 +1,28 @@
 import { Schema } from 'mongoose';
 
 export const heroeSchema = new Schema({
-  nombre: String,
-  nombreHeroe: String,
-  nombreActor: String,
-  trabajo: String,
-  bio: {
-    fechaPublicacion: Date,
-    lugarNacimiento: String,
-    primeraAparicion: String,
-    editor: String,
-    bando: String,
-  },
-  apariencia: {
-    sexo: String,
-    raza: String,
-    altura: String,
-    colorOjo: String,
-    colorCabello: String,
-  },
-  colaboradores: [String],
-  enemigo: [String],
-  img: [String],
-});
+    nombre: String,
+    nombreHeroe: String,
+    nombreActor: String,
+    trabajo: String,
+
+    bio: {
+        fechaPublicacion: Date,
+        lugarNacimiento: String,
+        primeraAparicion: String,
+        editor: String,
+        bando: String,
+    },
+    apariencia: {
+        sexo: String,
+        raza: String,
+        altura: String,
+        colorOjo: String,
+        colorCabello: String,
+    },
+    colaboradores: [String],
+    enemigo: [String],
+    superPoder: [String],
+    img: [String]
+})
+
